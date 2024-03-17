e156f5b9cad6a549b732506196db41fdeae18a80
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-17 22:00:51 +1030

    a sum function

--- BasicLogicQuestions/1-EasyLogic/PalindromeChecker.js
+++
@@ -1,3 +0,0 @@
-// Palindrome Checker:
-// Write a function that checks if a given string is a palindrome (reads the same forwards and backwards).
-
---
+++ BasicLogicQuestions/1-EasyLogic/Summation.js
@@ -0,0 +1,10 @@
+const Summation = (sum1, sum2) =>{
+
+
+
+    return console.log(sum1 + sum2)
+
+}
+
+
+Summation(1,4)
